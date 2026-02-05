b36e4fc7924c959c5e51e2f55ef4b1381fafe483
Link4Electronics <link4electronics@gmail.com>
2026-02-05 09:31:59 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,7 +17,7 @@ quick-sharun /usr/bin/nestopia /usr/share/nestopia
 #cp /usr/share/nestopia/NstDatabase.xml ./AppDir/bin
 #cp -r /usr/share/nestopia/palettes ./AppDir/bin
 #cp -r /usr/share/nestopia/shaders ./AppDir/bin
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/share/nestopia' >> ./AppDir/.env
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
