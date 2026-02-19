14dab22af67734b95255d5b589f1297704073442
Link4Electronics <link4electronics@gmail.com>
2026-02-19 19:29:36 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,6 +9,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/nestopia.svg
 export DESKTOP=/usr/share/applications/nestopia.desktop
+export STARTUPWMCLASS=nestopia
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
