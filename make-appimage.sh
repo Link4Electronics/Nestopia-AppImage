ccf888ae680cda6f46f0b7ce2ce6b01ede25bdc2
Link4Electronics <link4electronics@gmail.com>
2026-03-28 11:19:13 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,22 +5,18 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/scalable/apps/nestopia.svg
-export DESKTOP=/usr/share/applications/nestopia.desktop
-export STARTUPWMCLASS=nestopia
+export ICON=https://gitlab.com/jgemu/nestopia/-/raw/master/icons/nestopia256.png?ref_type=heads
+export DESKTOP=https://gitlab.com/jgemu/nestopia/-/raw/master/nestopia-jg.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/nestopia
-#echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the app normally quits before that time
 # then skip this or check if some flag can be passed that makes it stay open
-#quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
